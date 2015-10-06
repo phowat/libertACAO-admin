e9cdd251c214c35193bdab1b7fdb4ea73bf5b2dc
Pedro Howat <pedro.howat@gmail.com>
2015-10-06 00:19:39 -0300

    Silly login test

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,17 +1,8 @@
 Template.home.events({
     "submit form": function(e) {
         e.preventDefault();
-        var formData = $(this).serialize();
+        var formData = $(e.target).serializeArray();
         console.log("DELETEME: ->", formData);
-        Parse.User.logIn("myname", "mypass", {
-          success: function(user) {
-            // Do stuff after successful login.
-            console.log("DELETEME: user", user);
-          },
-          error: function(user, error) {
-            // The login failed. Check error to see why.
-            console.log("DELETEME: error", error);
-          }
-        });
+        Meteor.call("parseLogin", formData[0].value, formData[1].value);
     },
 });
