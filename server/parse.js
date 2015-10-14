6c5b1e8454a56d381323887cc50f439a9ca16fda
Pedro Howat <pedro.howat@gmail.com>
2015-10-14 00:03:47 -0300

    moving parse related code to package

--- server/parse.js
+++ server/parse.js
@@ -1,5 +1,7 @@
+/*
 Meteor.startup(function () {
     Parse = Meteor.npmRequire('parse/node').Parse;
     var conf = ParseConf.findOne();
     Parse.initialize(conf.applicationId, conf.jsKey);
 });
+*/
